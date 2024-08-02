5ee883e3a85ed9a7c7e587838fe2269a7988b4a4
Tinkal Das <tinkaldas00@gmail.com>
2024-08-02 10:37:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let skills=document.querySelector("#skills");
+let education=document.querySelector("#education");
+let projects=document.querySelector("#projects");
+let skillsInfo=document.querySelector("#skills-info");
+let educationInfo=document.querySelector("#education-info");
+let projectsInfo=document.querySelector("#projects-info");
+
+skills.addEventListener("click", ()=>{
+       skillsInfo.style.visibility="visible";
+       educationInfo.style.visibility="hidden";
+       projectsInfo.style.visibility="hidden";
+})
+education.addEventListener("click", ()=>{
+    educationInfo.style.visibility="visible";
+    projectsInfo.style.visibility="hidden";
+    skillsInfo.style.visibility="hidden";
+})
+projects.addEventListener("click", ()=>{
+    projectsInfo.style.visibility="visible";
+    educationInfo.style.visibility="hidden";
+    skillsInfo.style.visibility="hidden";
+})
